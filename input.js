5965fbc5eb122bc233bf8586a70f71a780d92a9c
yevhenii-vdovin <45859199+yevhenii-vdovin@users.noreply.github.com>
2021-08-03 20:35:02 +0200

    Update input.js

--- input.js
+++ input.js
@@ -1,21 +1,42 @@
 let inputDirection = { x: 0, y: 0 };
 
-window.addEventListener("keydown", (e) => {
-  switch (e.key) {
-    case "ArrowUp":
-      inputDirection = { x: 0, y: -1 };
-      break;
-    case "ArrowDown":
-      inputDirection = { x: 0, y: 1 };
-      break;
-    case "ArrowLeft":
-      inputDirection = { x: -1, y: 0 };
-      break;
-    case "ArrowRight":
-      inputDirection = { x: 1, y: 0 };
-      break;
-  }
+document.querySelector(".up").addEventListener("click", (e) => {
+  if (inputDirection.y !== 0) return;
+  e.tagrget = inputDirection = { x: 0, y: -1 };
+  return;
 });
+document.querySelector(".down").addEventListener("click", (e) => {
+  if (inputDirection.y !== 0) return;
+  e.tagrget = inputDirection = { x: 0, y: 1 };
+  return;
+});
+document.querySelector(".left").addEventListener("click", (e) => {
+  if (inputDirection.x !== 0) return;
+  e.tagrget = inputDirection = { x: -1, y: 0 };
+  return;
+});
+document.querySelector(".right").addEventListener("click", (e) => {
+  if (inputDirection.x !== 0) return;
+  e.tagrget = inputDirection = { x: 1, y: 0 };
+  return;
+});
+
+// window.addEventListener("keydown", (e) => {
+//   switch (e.key) {
+//     case "ArrowUp":
+//       inputDirection = { x: 0, y: -1 };
+//       break;
+//     case "ArrowDown":
+//       inputDirection = { x: 0, y: 1 };
+//       break;
+//     case "ArrowLeft":
+//       inputDirection = { x: -1, y: 0 };
+//       break;
+//     case "ArrowRight":
+//       inputDirection = { x: 1, y: 0 };
+//       break;
+//   }
+// });
 
 export function getInputDirection() {
   return inputDirection;
